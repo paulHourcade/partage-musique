1165cc1a2d2d058b4c2d54f43ec8d5594620cfee
paulHourcade <44840190+paulHourcade@users.noreply.github.com>
2026-04-13 21:27:26 +0200

    Update ScrollToTop.jsx

--- src/ScrollToTop.jsx
+++ src/ScrollToTop.jsx
@@ -2,15 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "auto",
-    });
-  }, [pathname]);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   return null;
 }
